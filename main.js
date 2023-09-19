46d88ce3ef13d933385241a6368679fe7ea712ee
kirurr <timon09df@gmail.com>
2023-09-19 17:32:45 +0300

    yoy

--- main.js
+++ main.js
@@ -16,50 +16,6 @@ fetch('menu.json')
       menuItemA.classList.add('has-child');
       checkSubMenu(e, menuItem);
     }
-
-    function checkSubMenu(e, menuCover) {
-
-      let subMenuCover = document.createElement('ul');
-      subMenuCover.classList.add('sub-menu-cover');
-      console.log(menuCover);
-
-      menuCover.appendChild(subMenuCover);
-
-      if (e.subMenu) {
-        Object.values(e.subMenu).forEach((i) => {
-
-          let subMenuItem = document.createElement('li');
-
-          let subMenuItemA = document.createElement('a');
-
-          function makeSubMenuContent(e, menuCover, menuItem, menuItemA) {
-            if (!menuItem) {
-              let menuItem = document.createElement('li');
-            }
-            if (!menuItemA) {
-              let MenuItemA = document.createElement('a');
-            }
-            menuCover.appendChild(menuItem);
-
-            menuItemA.href = e.url;
-            menuItemA.textContent = e.name;
-
-            menuItem.appendChild(menuItemA);
-          }
-
-          makeSubMenuContent(i, subMenuCover, subMenuItem, subMenuItemA);
-
-          if (i.subMenu) {
-            subMenuItemA.classList.add('has-child');
-            let subMenuCover = document.createElement('ul');
-            subMenuCover.classList.add('sub-menu-cover');
-
-            checkSubMenu(i, subMenuItem)
-          }
-
-        })
-      }
-    };
   })
   )
   .then(() => {
@@ -73,6 +29,49 @@ fetch('menu.json')
       });
     }
   })
+
+  function checkSubMenu(e, menuCover) {
+
+    let subMenuCover = document.createElement('ul');
+    subMenuCover.classList.add('sub-menu-cover');
+
+    menuCover.appendChild(subMenuCover);
+
+    if (e.subMenu) {
+      Object.values(e.subMenu).forEach((i) => {
+
+        let subMenuItem = document.createElement('li');
+
+        let subMenuItemA = document.createElement('a');
+
+        function makeSubMenuContent(e, menuCover, menuItem, menuItemA) {
+          if (!menuItem) {
+            let menuItem = document.createElement('li');
+          }
+          if (!menuItemA) {
+            let MenuItemA = document.createElement('a');
+          }
+          menuCover.appendChild(menuItem);
+
+          menuItemA.href = e.url;
+          menuItemA.textContent = e.name;
+
+          menuItem.appendChild(menuItemA);
+        }
+
+        makeSubMenuContent(i, subMenuCover, subMenuItem, subMenuItemA);
+
+        if (i.subMenu) {
+          subMenuItemA.classList.add('has-child');
+          let subMenuCover = document.createElement('ul');
+          subMenuCover.classList.add('sub-menu-cover');
+
+          checkSubMenu(i, subMenuItem)
+        }
+
+      })
+    }
+  };
 
 
 
